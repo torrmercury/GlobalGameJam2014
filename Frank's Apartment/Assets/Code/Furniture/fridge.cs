b126d96f39ead65aec56038dc48dfd9af5db3155
Matt Chen <matt.chen312@gmail.com>
2014-01-25 15:49:08 -0500

    blah

---
+++ Frank's Apartment/Assets/Code/Furniture/fridge.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class fridge : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseDown()
+	{
+		//play open fridge
+	}
+}
