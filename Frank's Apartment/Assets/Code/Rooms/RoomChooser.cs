0d72bd07b533aec18f5915b3c7c48279e872a69f
Matt Chen <matt.chen312@gmail.com>
2014-01-24 22:34:23 -0500

    Initial creation and file structure. Created frameworks for Rooms and TenantAI.

---
+++ Frank's Apartment/Assets/Code/Rooms/RoomChooser.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RoomChooser : MonoBehaviour {
+	int x;
+	public int deadBodies;
+
+	// Use this for initialization
+	void Start ()
+	{
+		if (deadBodies < 5)
+		{
+			x = deadBodies;
+		}
+		if (x == 0)
+		{
+			Instantiate (bathroom);
+
+		}
+		else if (x == 1)
+		{
+			Instantiate (bedroom);
+			
+		}
+		else if (x == 2)
+		{
+			Instantiate (kitchen);
+			
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Frank's Apartment/Assets/Code/Tenant/TenantAI.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TenentAI : MonoBehaviour {
+	bool dead = 0;
+	public int deadBodies;
+	int bodies;
+	// Use this for initialization
+	void Start ()
+	{
+		bodies = deadBodies;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (dead = 1)
+		{
+			bodies += 1;
+			deadBodies = bodies;
+			Instantiate (body);
+			Destroy (this);
+		}
+	}
+}
