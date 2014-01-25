733cdb916d26ccd8033e9bff72db91c234cb51bc
Johnny! <firehyren@gmail.com>
2014-01-24 22:54:48 -0500

    Started work on AI

--- Frank's Apartment/Assets/Code/Tenant/TenantAI.cs
+++ Frank's Apartment/Assets/Code/Tenant/TenantAI.cs
@@ -2,20 +2,34 @@
 using System.Collections;
 
 public class TenentAI : MonoBehaviour {
+
+	int Health = 5;
+	int[] Goals = new int[hunger, sleep, sex, excretion];
+	string Objective;
+	
 	bool dead = 0;
 	public int deadBodies;
 	int bodies;
+	
 	// Use this for initialization
-	void Start ()
-	{
+	void Start () {
+		//array of goals
+		//hunger, sleep, sex, excretion
+		//choose path
+
+
 		bodies = deadBodies;
+		ChooseGoal();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		//start walking path
+		//choose new path if interrupted
+			//Interruption
 
-		if (dead = 1)
-		{
+
+		if (dead = 1) {
 			bodies += 1;
 			deadBodies = bodies;
 			Instantiate (body);
@@ -23,3 +37,10 @@ public class TenentAI : MonoBehaviour {
 		}
 	}
 }
+
+
+void ChooseGoal () {
+	int Goal = Random.Range (0, 100);
+	if (0 <= Goal <= 24) {
+		Objective = 'hunger';
+		else if (25 <= 
