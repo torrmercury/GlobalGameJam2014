1e8849c42e1d8b0d699470874cccbffa93a16a7e
Matt Chen <matt.chen312@gmail.com>
2014-01-25 18:01:00 -0500

    Door now works

--- Frank's Apartment/Assets/Code/Furniture/door.cs
+++ Frank's Apartment/Assets/Code/Furniture/door.cs
@@ -3,15 +3,26 @@ using System.Collections;
 
 public class door : MonoBehaviour {
     bool clicked = false;
+    bool locked = false;
 	// Use this for initialization
 	void Start () {
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update ()
+    {
+	    //if locked character cannot open door
+        if (locked == false)
+        {
+            collider.enabled = false;
+        }
+        else if (locked == true)
+        {
+            collider.enabled = true;
+        }
 	}
+
     void OnMouseDown()
     {
         if (clicked == false)
@@ -25,11 +36,11 @@ public class door : MonoBehaviour {
 
         if (clicked == false)
         {
-
+            locked = false;
         }
         else if (clicked == true)
         {
-
+            locked = true;
         }
     }
 }
