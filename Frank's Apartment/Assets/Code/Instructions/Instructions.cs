0fabd9507c02b19abf9d256d0af000c5babbc970
Core <seorisachs@gmail.com>
2014-01-26 16:03:22 -0500

    the instructions

---
+++ Frank's Apartment/Assets/Code/Instructions/Instructions.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Instructions : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Destroy(gameObject, 10);
+	}
+
+	void OnGUI() {
+		GUI.Box(new Rect (100, 100, 300, 100), "You are the apartment.\n You have the power.\n All tenants must die.");{
+		
+n		}
+	
+	}
+}
