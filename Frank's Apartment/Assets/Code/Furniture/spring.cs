58982b3999e3bd65e7dfa61f03756fad0f2b3491
Matt Chen <matt.chen312@gmail.com>
2014-01-26 13:32:32 -0500

    Created title screen and GUI ability

--- Frank's Apartment/Assets/Code/Furniture/spring.cs
+++ Frank's Apartment/Assets/Code/Furniture/spring.cs
@@ -2,23 +2,22 @@
 using System.Collections;
 
 public class spring : MonoBehaviour {
-    private Animation Animation;
+    //private Animation Animation;
     public spring spawnspring;
 
 	// Use this for initialization
 	void Start () {
         //StartCoroutine("Spring");
-        Animation = GetComponent<Animation>();
-        Animation.Stop();
+        /*Animation = GetComponent<Animation>();
+        Animation.Stop();*/
 	}
 	
 	// Update is called once per frame
     void Update()
     {
-        
-        if (couch.couchClicked == true && Animation.isPlaying == false)
+        if (couch.couchClicked == true && animation.isPlaying == false)
         {
-            Animation.Play("Spring");
+            animation.Play();
             couch.couchClicked = false;
             StartCoroutine("SpringReset");
             Debug.Log("If statement works");
@@ -29,7 +28,7 @@ public class spring : MonoBehaviour {
     IEnumerator SpringReset()
     {
         yield return new WaitForSeconds(3f);
-        Animation.Rewind();
+        animation.Rewind();
         Debug.Log("coroutine works");
     }
 }
