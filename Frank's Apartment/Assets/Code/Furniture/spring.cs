f501cc4e7c863c2c48584a752097f1190d52e046
Matt Chen <matt.chen312@gmail.com>
2014-01-26 11:43:01 -0500

    Trying to fix spring

--- Frank's Apartment/Assets/Code/Furniture/spring.cs
+++ Frank's Apartment/Assets/Code/Furniture/spring.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class spring : MonoBehaviour {
     private Animation Animation;
+    public spring spawnspring;
 
 	// Use this for initialization
 	void Start () {
@@ -13,18 +14,21 @@ public class spring : MonoBehaviour {
 	// Update is called once per frame
     void Update()
     {
-        Debug.Log("test");
-        if (couch.couchClicked == false && Animation.isPlaying == true)
-        {
-            //Animation.Stop();
-            //Animation.Rewind("Spring");
-        }
-        
-        else if (couch.couchClicked == true && Animation.isPlaying == false)
+        if (couch.couchClicked == true && Animation.isPlaying == false)
         {
             Animation.Play("Spring");
             couch.couchClicked = false;
+            StartCoroutine("SpringGo");
         }
-        
+    }
+
+    IEnumerator SpringGo()
+    {
+        Vector3 origPos = transform.position;
+        Debug.Log(origPos);
+        yield return new WaitForSeconds(3f);
+        spring newspring;
+        newspring = Instantiate(spawnspring, origPos, Quaternion.identity) as spring;
+        Destroy(gameObject);
     }
 }
