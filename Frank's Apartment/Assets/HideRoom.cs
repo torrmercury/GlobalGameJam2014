9981a866782d1a3d491232f177abce467abb93a0
Roman <pencilking2002@gmail.com>
2014-01-26 11:24:26 -0500

    Started writing HideRoom script

--- Frank's Apartment/Assets/HideRoom.cs
+++ Frank's Apartment/Assets/HideRoom.cs
@@ -2,10 +2,13 @@
 using System.Collections;
 
 public class HideRoom : MonoBehaviour {
-
+	
+	Renderer[] ChildrenRenderer;
+	
 	// Use this for initialization
 	void Start () {
-	
+		ChildrenRenderer = transform.GetComponentsInChildren<Renderer>();
+		print ("Renderers: " + ChildrenRenderer[0]);
 	}
 	
 	// Update is called once per frame
